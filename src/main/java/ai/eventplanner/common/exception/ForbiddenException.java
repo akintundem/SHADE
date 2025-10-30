b0ce12aa0039fae5c58cb4290d1e3814c91c55bb
Mayokun Moses Akintunde <akintundemayokun@gmail.com>
2025-10-30 18:53:26 -0500

    refactor: Simplify ForbiddenException by extending RuntimeException instead of ApiException

--- src/main/java/ai/eventplanner/common/exception/ForbiddenException.java
+++ src/main/java/ai/eventplanner/common/exception/ForbiddenException.java
@@ -1,13 +1,5 @@
 package ai.eventplanner.common.exception;
 
-public class ForbiddenException extends ApiException {
-    public ForbiddenException(String code, String message) {
-        super(code, message, 403);
-    }
-}
-
-package ai.eventplanner.common.exception;
-
 public class ForbiddenException extends RuntimeException {
     
     public ForbiddenException(String message) {
